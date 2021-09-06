4671737cc5def8f074da5fa01407a064d70a622e
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-06 15:07:22 +0300

    add banner

--- src/components/pages/culture/Culture.jsx
+++ src/components/pages/culture/Culture.jsx
@@ -1,9 +1,17 @@
 import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+import CultureBanner from './culture_components/CultureBanner';
+
+const useStyles = makeStyles((theme) => ({}));
 
 const Culture = () => {
+    const classes = useStyles();
+
     return (
         <div>
-            <Container maxWidth='lg'>Culture page is here</Container>
+            <Container maxWidth='lg'>
+                <CultureBanner />
+            </Container>
         </div>
     );
 };
---
+++ src/components/pages/culture/culture_components/CultureBanner.jsx
@@ -0,0 +1,76 @@
+import { makeStyles, Typography } from '@material-ui/core';
+import ImageBanner from '../../../../images/cultureBanner.jpeg';
+
+const useStyles = makeStyles((theme) => ({
+    banner: {
+        backgroundColor: '#ccc',
+        height: '100%',
+        backgroundImage: `url(${ImageBanner})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+    },
+    bannerText: {
+        width: '50%',
+        padding: 48,
+        color: '#fff',
+        [theme.breakpoints.down('sm')]: {
+            width: '80%',
+            padding: 30,
+        },
+    },
+    bannerTitle: {
+        marginBottom: 16,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '30px',
+        },
+    },
+    bannerSubtitle: {
+        fontSize: 24,
+        marginBottom: 24,
+        lineHeight: '32px',
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '18px',
+        },
+    },
+    bannerLinkText: {
+        color: '#1976D2',
+        cursor: 'pointer',
+    },
+}));
+
+const CultureBanner = () => {
+    const classes = useStyles();
+
+    return (
+        <>
+            <div className={classes.banner}>
+                <div className={classes.bannerText}>
+                    <Typography
+                        variant='h3'
+                        component='h1'
+                        className={classes.bannerTitle}
+                    >
+                        Culture is the widening of the mind and of the spirit
+                    </Typography>
+                    <Typography
+                        variant='subtitle1'
+                        className={classes.bannerSubtitle}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dolorem illum, doloribus architecto accusamus libero
+                        quasi repudiandae a ratione voluptatum. Laborum.
+                    </Typography>
+                    <Typography
+                        variant='body1'
+                        className={classes.bannerLinkText}
+                    >
+                        Continue reading…
+                    </Typography>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default CultureBanner;
